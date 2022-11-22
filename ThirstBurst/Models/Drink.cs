4d814871d21746c63aeb28103a38a7eb56d0001c
Hamza <afshanshoukat.bcs19.011@gmail.com>
2022-11-22 17:47:28 +0500

    Add project files.

---
+++ ThirstBurst/Models/Drink.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThirstBurst.Models
+{
+    public class Drink
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Image_url { get; set; }
+    
+    }
+}
